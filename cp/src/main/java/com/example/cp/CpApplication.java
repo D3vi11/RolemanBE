6260060881039d555477835bc0a232e376a5e429
D3vi11 <krystianb69@gmail.com>
2023-12-02 21:04:46 +0100

    adding discovery server/client base configuration

--- cp/src/main/java/com/example/cp/CpApplication.java
+++ cp/src/main/java/com/example/cp/CpApplication.java
@@ -2,8 +2,10 @@ package com.example.cp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CpApplication {
 
     public static void main(String[] args) {
--- gen/src/main/java/com/example/gen/GenApplication.java
+++ gen/src/main/java/com/example/gen/GenApplication.java
@@ -2,8 +2,10 @@ package com.example.gen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GenApplication {
 
     public static void main(String[] args) {
