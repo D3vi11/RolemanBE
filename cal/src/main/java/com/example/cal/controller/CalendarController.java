a6cd30c747e11975e89a93c6cc9942995336146a
D3vi11 <krystianb69@gmail.com>
2023-12-08 23:44:42 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- cal/src/main/java/com/example/cal/controller/CalendarController.java
+++ cal/src/main/java/com/example/cal/controller/CalendarController.java
@@ -1,14 +1,18 @@
 package com.example.cal.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("")
 public class CalendarController {
+
+    @Value("${mongo.password}")
+    private String value;
     @GetMapping
     public String getCalendar(){
-        return "calendar";
+        return "calendar "+value;
     }
     @PostMapping
     public void addCalendar(){
