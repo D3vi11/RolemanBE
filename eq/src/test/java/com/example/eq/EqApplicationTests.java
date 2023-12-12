2c2d4b35a13eb32136d6c7984138097990a1557b
D3vi11 <krystianb69@gmail.com>
2023-12-12 22:56:11 +0100

    fix

--- eq/src/test/java/com/example/eq/EqApplicationTests.java
+++ eq/src/test/java/com/example/eq/EqApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EqApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
--- mp/src/test/java/com/example/mp/MpApplicationTests.java
+++ mp/src/test/java/com/example/mp/MpApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MpApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
