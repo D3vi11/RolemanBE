941e57ccc39b2033db5b72581100ed6e721e842c
D3vi11 <krystianb69@gmail.com>
2023-12-14 17:51:45 +0100

    auth prep

--- auth/src/main/java/com/example/auth/controller/AuthController.java
+++ auth/src/main/java/com/example/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.auth.controller;
 
+import com.example.auth.dto.LoginDto;
 import com.example.auth.dto.RegisterDto;
 import com.example.auth.service.UserService;
 import jakarta.validation.Valid;
@@ -17,26 +18,34 @@ public class AuthController {
 
     private UserService userService;
 
-    @GetMapping
-    public String test(){
-        return "Authorization";
-    }
-
     @PostMapping("login")
-    public void login(){
-
+    public ResponseEntity<String> login(@Valid @RequestBody LoginDto loginDto){
+        return new ResponseEntity<>("Work in progress",HttpStatus.I_AM_A_TEAPOT);
     }
     @PostMapping("register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterDto registerDto){
         userService.registerUser(registerDto);
         return new ResponseEntity<>("Rejestracja się powiodła, przesłano maila na "+registerDto.getEmail()+". Konto należy potwierdzić w ciągu 7 dni albo zostanie usunięte ", HttpStatus.OK);
     }
-    @PutMapping
-    public void update(){
+    @PostMapping("validate")
+    public ResponseEntity<String> validateToken(@RequestBody String token){
+        return new ResponseEntity<>("Work in progress",HttpStatus.I_AM_A_TEAPOT);
+    }
 
+    @PostMapping("token")
+    public ResponseEntity<String> refreshToken(){
+        return new ResponseEntity<>("Work in progress",HttpStatus.I_AM_A_TEAPOT);
+    }
+    @GetMapping("confirm")
+    public ResponseEntity<String> confirmAccount(@RequestBody String token){
+        return new ResponseEntity<>("Work in progress",HttpStatus.I_AM_A_TEAPOT);
+    }
+    @PutMapping
+    public ResponseEntity<String> update(){
+        return new ResponseEntity<>("Work in progress",HttpStatus.I_AM_A_TEAPOT);
     }
     @DeleteMapping
-    public void delete(){
-
+    public ResponseEntity<String> delete(){
+        return new ResponseEntity<>("Work in progress",HttpStatus.I_AM_A_TEAPOT);
     }
 }
