1eb1c7babc4d067f893f53b5a05035fb9b198b88
D3vi11 <krystianb69@gmail.com>
2023-12-22 23:35:18 +0100

    adding gateway authorization

--- api-gateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -1,10 +1,15 @@
 package com.example.apigateway.config;
 
 import com.example.apigateway.filter.AuthorizationFilter;
+import org.json.JSONObject;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
@@ -16,28 +21,35 @@ public class GatewayConfig {
                                 .filter(authorizationFilter))
                         .uri("lb://character"))
                 .route("campaign", r -> r.path("/campaign/**")
-                        .filters(f->f.rewritePath("/campaign","/"))
+                        .filters(f->f.rewritePath("/campaign","/")
+                                .filter(authorizationFilter))
                         .uri("lb://campaign"))
                 .route("data", r -> r.path("/data/**")
-                        .filters(f->f.rewritePath("/data","/"))
+                        .filters(f->f.rewritePath("/data","/")
+                                .filter(authorizationFilter))
                         .uri("lb://data"))
                 .route("equipment", r -> r.path("/equipment/**")
-                        .filters(f->f.rewritePath("/equipment","/"))
+                        .filters(f->f.rewritePath("/equipment","/")
+                                .filter(authorizationFilter))
                         .uri("lb://equipment"))
                 .route("generator", r -> r.path("/generator/**")
-                        .filters(f->f.rewritePath("/generator","/"))
+                        .filters(f->f.rewritePath("/generator","/")
+                                .filter(authorizationFilter))
                         .uri("lb://generator"))
                 .route("map", r -> r.path("/map/**")
-                        .filters(f->f.rewritePath("/map","/"))
+                        .filters(f->f.rewritePath("/map","/")
+                                .filter(authorizationFilter))
                         .uri("lb://map"))
                 .route("weather", r -> r.path("/weather/**")
-                        .filters(f->f.rewritePath("/weather","/"))
+                        .filters(f->f.rewritePath("/weather","/")
+                                .filter(authorizationFilter))
                         .uri("lb://weather"))
                 .route("authorization", r -> r.path("/authorization/**")
                         .filters(f->f.rewritePath("/authorization","/"))
                         .uri("lb://authorization"))
                 .route("calendar", r -> r.path("/calendar/**")
-                        .filters(f->f.rewritePath("/calendar","/"))
+                        .filters(f->f.rewritePath("/calendar","/")
+                                .filter(authorizationFilter))
                         .uri("lb://calendar"))
                 .build();
     }
--- api-gateway/src/main/java/com/example/apigateway/filter/AuthorizationFilter.java
+++ api-gateway/src/main/java/com/example/apigateway/filter/AuthorizationFilter.java
@@ -5,33 +5,50 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class AuthorizationFilter implements GatewayFilter, Ordered {
 
     @Value("${auth.url}")
     private String authUrl;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         WebClient webClient = WebClient.create(authUrl);
-        String response = webClient
+        if(!exchange.getRequest().getHeaders().containsKey("Authorization")){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        if(exchange.getRequest().getHeaders().get("Authorization").isEmpty()){
+           exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+           return exchange.getResponse().setComplete();
+        }
+        return webClient
                 .get()
                 .uri("/validate")
+                .header("Authorization", exchange.getRequest().getHeaders().get("Authorization").get(0))
                 .retrieve()
                 .bodyToMono(String.class)
-                .block();
-        JSONObject jsonObject = new JSONObject(response);
-        Boolean isValid = (Boolean)jsonObject.get("valid");
-        if(!isValid){
-            throw new IncorrectTokenException("Token jest niepoprawny");
-        }
-        return chain.filter(exchange);
+                .flatMap(value -> {
+                    JSONObject jsonObject = new JSONObject(value);
+                    Boolean isValid = (Boolean) jsonObject.get("valid");
+                    if (!isValid) {
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    }
+                    return chain.filter(exchange);
+                });
     }
 
     @Override
